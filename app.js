4b8755730a732b39f1a42cad51951dcf76214c4b
aryavish <aryav2010@gmail.com>
2023-01-21 19:05:48 -0500

    ee

---
+++ app.js
@@ -0,0 +1,12 @@
+(function () {
+    'use strict';
+
+    var x = 4;
+
+
+    angular.module('myFirstApp', [])
+
+        .controller('MyFirstController', function () { })
+
+    console.log("fuck my ass")
+})();
